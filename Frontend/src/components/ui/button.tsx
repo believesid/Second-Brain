0d17c2d681bc99a9159af92e7fb004a761573172
Siddharth Nayak <nayaksiddharth2004@gmail.com>
2026-04-27 18:32:01 +0530

    folder structure sorted

---
+++ Frontend/src/components/ui/button.tsx
@@ -0,0 +1,15 @@
+
+{/* <button className="bg-blue-600 text-white px-4 py-2 rounded">
+  Click me
+</button> */}
+
+
+// const Button: React.FC = () => {
+//   return (
+//     <button className="counter">
+//       Click me
+//     </button>
+//   );
+// };
+
+// export default Button;
