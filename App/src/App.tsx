69f4ceb839460fae19f66028a6ce8881cfe808a1
Siddharth Nayak <nayaksiddharth2004@gmail.com>
2026-05-07 22:41:55 +0530

    card component created

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -3,20 +3,29 @@ import './App.css'
 import { Button } from './components/Button'
 import { PlusIcon } from './icons/PlusIcon'
 import {ShareIcon} from './icons/ShareIcon'
+import { Card } from './components/Card'
 
 function App() {
-return <div className='flex'>
+return <div>
   
-  <Button 
+  <div className='flex justify-end gap-4 ' >
+     <Button
   //@ts-ignore
   startIcon={<PlusIcon />} 
-  variant = "primary" text = "Add content"> 
+  variant = "primary" text = "Add content" size='sm'> 
   endIcon={ <ShareIcon/>}
   </Button>
   <Button
   //@ts-ignore
   startIcon={<ShareIcon />}
-   variant = "secondary" text = "Share"></Button>
+   variant = "secondary" text = "Share Brain" size='sm'></Button>
+  
+  </div>
+ 
+ <div>
+     <Card/>
+ </div>
+ 
   
 </div>
 }
